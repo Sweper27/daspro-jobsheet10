eb0ff183eedbeba1573bfd7b41b67897e7e560cc
Sweper27 <rifqiariessaputra@gmail.com>
2024-11-07 10:06:12 +0700

    mengubah syntax untuk menjawab pertanyaan

--- SIAKAD26.java
+++ SIAKAD26.java
@@ -4,29 +4,40 @@ public class SIAKAD26 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[][] nilai = new int [4][3];
+        System.out.print("Masukkan jumlah siswa: ");
+        int jumlahSiswa = sc.nextInt();
+        
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMataKuliah = sc.nextInt();
 
-        for (int i = 0; i < nilai.length;i++) {
-            System.out.println("Input nilai mahasiswa ke-" + (i+1));
+        
+        int[][] nilai = new int[jumlahSiswa][jumlahMataKuliah];
+
+        
+        for (int i = 0; i < jumlahSiswa; i++) {
+            System.out.println("\nInput nilai mahasiswa ke-" + (i + 1));
             double totalperSiswa = 0;
 
-            for (int j=0; j < nilai[i].length;j++) {
-                System.out.print("Nilai mata kuliah " + (j+1) + ": ");
+            for (int j = 0; j < jumlahMataKuliah; j++) {
+                System.out.print("Nilai mata kuliah " + (j + 1) + ": ");
                 nilai[i][j] = sc.nextInt();
-                totalperSiswa += nilai [i][j];
+                totalperSiswa += nilai[i][j];
             }
-            System.out.println("Nilai rata-rata; " + totalperSiswa/3);
+            System.out.println("Nilai rata-rata mahasiswa ke-" + (i + 1) + ": " + totalperSiswa / jumlahMataKuliah);
         }
-        System.out.println("========================================");
+
+        
+        System.out.println("\n========================================");
         System.out.println("Rata-rata Nilai setiap Mata kuliah:");
 
-        for (int j=0; j <3; j++) {
+        for (int j = 0; j < jumlahMataKuliah; j++) {
             double totalperMatkul = 0;
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < jumlahSiswa; i++) {
                 totalperMatkul += nilai[i][j];
             }
-            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalperMatkul / 4 );
+            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalperMatkul / jumlahSiswa);
         }
+        
     }
 }
